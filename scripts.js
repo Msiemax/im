94768c3c3caea3b23b433dba11f53433d6bf7dbb
Msiemax <38659429+Msiemax@users.noreply.github.com>
2018-12-16 14:16:28 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('right').addEventListener('click', function(){
+	document.querySelector('.bg-model').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+	document.querySelector('.bg-model').style.display = 'none';
+});
+
